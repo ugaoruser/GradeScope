c59c89f5f3e62acc332885e0f0e121db0ecf66d8
ugaoruser <jackdylangallentes22@gmail.com>
2025-10-11 22:14:44 +0800

    deleted admin account from seed_users.sql

--- server/seed_users.sql
+++ server/seed_users.sql
@@ -7,4 +7,3 @@ VALUES
 ('mike.johnson@school.edu', '$2b$12$LQv3c1yqBWVHxkd0LHAkCOYz6TtxMQJqhN8/LewdBPj4J/HSy.8K2', 'Mike Johnson', 2), -- teacher
 ('sarah.wilson@school.edu', '$2b$12$LQv3c1yqBWVHxkd0LHAkCOYz6TtxMQJqhN8/LewdBPj4J/HSy.8K2', 'Sarah Wilson', 2), -- teacher
 ('robert.brown@school.edu', '$2b$12$LQv3c1yqBWVHxkd0LHAkCOYz6TtxMQJqhN8/LewdBPj4J/HSy.8K2', 'Robert Wilson', 3), -- parent
-('admin@school.edu', '$2b$12$LQv3c1yqBWVHxkd0LHAkCOYz6TtxMQJqhN8/LewdBPj4J/HSy.8K2', 'Admin User', 4); -- admin
